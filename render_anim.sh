4d2df8f7dab3e03c8eb68061fa32c57793136be7
OHMORIYUSUKE <ohmoriyusuke@gmail.com>
2021-09-16 17:09:06 +0900

    Fix

--- render_anim.sh
+++ render_anim.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# Blenderをレンダリング
+# Blenderをレンダリング CYCLES
 # https://docs.blender.org/manual/en/latest/advanced/command_line/arguments.html
-sudo blender --background -noaudio blend/Miraikomachi.blend --threads 0 -E CYCLES --render-output img/anim -s 1000 -e 1030 -a
+sudo blender --background -noaudio blend/Miraikomachi.blend --threads 0 -E EEVEE --render-output img/anim -s 1000 -e 1030 -a
 
 # 動画生成
 sudo pip install opencv-python-headless==4.4.0.44
