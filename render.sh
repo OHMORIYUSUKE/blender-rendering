9f548dcdc68794b9181c57945135e7f41be6b565
OHMORIYUSUKE <ohmoriyusuke@gmail.com>
2021-09-16 18:39:46 +0900

    Fix

--- render.sh
+++ render.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # 触るな
 # Blenderをレンダリング
-# https://docs.blender.org/manual/en/latest/advanced/command_line/arguments.html CYCLES
-sudo blender --background -noaudio blend/Miraikomachi.blend --threads 0 -E CYCLES --render-output img/anim --render-frame 2310
+# https://docs.blender.org/manual/en/latest/advanced/command_line/arguments.html CYCLES BLENDER_EEVEE
+sudo blender --background -noaudio blend/Miraikomachi.blend --threads 0 -E BLENDER_EEVEE --render-output img/anim --render-frame 2310
