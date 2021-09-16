5123b11738daa1155a87ccb1c30b464bcd5c90a4
OHMORIYUSUKE <ohmoriyusuke@gmail.com>
2021-09-16 13:21:32 +0900

    Python test

--- pngtomp4.py
+++ pngtomp4.py
@@ -2,30 +2,19 @@ import sys
 import cv2
 
 def main():
-    # encoder(for mp4)
-    fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
-    # output file name, encoder, fps, size(fit to image size)
-    video = cv2.VideoWriter('video.mp4',fourcc, 20.0, (1240, 1360))
+    img_array = []
+    for filename in sorted(glob.glob("img/*.png")):
+        img = cv2.imread(filename)
+        height, width, layers = img.shape
+        size = (width, height)
+        img_array.append(img)
 
-    if not video.isOpened():
-        print("can't be opened")
-        sys.exit()
+    name = 'project.mp4'
+    out = cv2.VideoWriter(name, cv2.VideoWriter_fourcc(*'MP4V'), 5.0, size)
 
-    for i in range(0, 10+1):
-        # hoge0000.png, hoge0001.png,..., hoge0090.png
-        img = cv2.imread('./img/anim%04d.png' % i)
-
-        # can't read image, escape
-        if img is None:
-            print("can't read")
-            break
-
-        # add
-        video.write(img)
-        print(i)
-
-    video.release()
-    print('written')
+    for i in range(len(img_array)):
+        out.write(img_array[i])
+    out.release()
 
 if __name__=="__main__":
     main()
