e4994ff1f9599f46d7faf76be9530120869946eb
OHMORIYUSUKE <ohmoriyusuke@gmail.com>
2021-09-18 16:10:56 +0900

    Add action

--- make.sh
+++ make.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sudo blender --background -noaudio blend/Miraikomachi.blend --threads 0 -E CYCLES --render-output img/anim -s 1798 -e 1799 -a
+sudo blender --background -noaudio blend/Miraikomachi.blend --threads 0 -E CYCLES --render-output img/anim -s 1980 -e 1999 -a
 
 sudo apt-get install -y -q python3
 
