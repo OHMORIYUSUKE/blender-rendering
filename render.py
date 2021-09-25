112407fe912a4ffcf3dae4bc611dad2923a854fc
OHMORIYUSUKE <ohmoriyusuke@gmail.com>
2021-09-25 13:30:06 +0900

    Python 対応

--- render.py
+++ render.py
@@ -10,16 +10,20 @@ def main():
             break
 
     cmdString = "#!/bin/sh"
-    if dict_cmd['VIDEO'] == '0':
+    # Pythonを使う場合
+    if dict_cmd['PYTHONFILENAME'] != '0':
         cmdString = cmdString + "\n\n"
-        cmdString = cmdString + "sudo blender --background -noaudio blend/" + dict_cmd['FILENAME'] + " --threads 0 -E CYCLES --render-output img/anim" + " -s " + dict_cmd['START'] + " -e " + dict_cmd['END'] + " -a"
+        cmdString = cmdString + "sudo blender --background -noaudio blend/" + dict_cmd['FILENAME'] + " --threads 0 -E CYCLES --python python/" + dict_cmd['PYTHONFILENAME'] + " --render-output img/anim" + " -s " + dict_cmd['START'] + " -e " + dict_cmd['END'] + " -a"
     else:
         cmdString = cmdString + "\n\n"
         cmdString = cmdString + "sudo blender --background -noaudio blend/" + dict_cmd['FILENAME'] + " --threads 0 -E CYCLES --render-output img/anim" + " -s " + dict_cmd['START'] + " -e " + dict_cmd['END'] + " -a"
+    # 動画を出力する場合
+    if dict_cmd['VIDEO'] == '1':
         cmdString = cmdString + "\n\n"
         cmdString = cmdString + "sudo pip install opencv-python-headless==4.4.0.44"
         cmdString = cmdString + "\n\n"
         cmdString = cmdString + "sudo python3 pngtomp4.py"
+
 
     text_file = open("make.sh", "wt")
     text_file.write(cmdString)
